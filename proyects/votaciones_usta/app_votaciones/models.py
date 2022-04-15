86b4d48847451814b8674a8f6af4ef54ef218bd8
Johann Castellanos <stevkast@gmail.com>
2022-04-15 17:44:47 -0500

    idk what is migrations, help

--- proyects/votaciones_usta/app_votaciones/models.py
+++ proyects/votaciones_usta/app_votaciones/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=45, null=False)
+    last_name = models.CharField(max_length=45, null=False)
+    email = models.CharField(max_length=45, null=False)
+    password = models.CharField(max_length=45, null=False)
+    is_superuser = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+    class Meta:
+        app_label = 'app_votaciones'
